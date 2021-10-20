f90340d03b9b961827e60ec80180cfd26b97a8cd
Chrinkus <seeschickrun@gmail.com>
2021-10-20 13:28:47 -0500

    Cleaned up some of the initial 2019 submissions
    Day 1 is good, day 2 was incomplete from copy-over. I re-wrote it and it
    runs FAST!

--- 2019/c/1901.c
+++ 2019/c/1901.c
@@ -1,3 +1,4 @@
+// Old file, holds up!
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,25 +8,24 @@
 
 int32_t fuel_for_fuel(int32_t mass)
 {
-    int32_t fuel = CALC_FUEL(mass);
-    return fuel > 0 ? fuel + fuel_for_fuel(fuel) : 0;
+	int32_t fuel = CALC_FUEL(mass);
+	return fuel > 0 ? fuel + fuel_for_fuel(fuel) : 0;
 }
 
 int main(void)
 {
-    printf("Advent of Code 2019 - Day 1\n");
-    printf("The Tyranny of the Rocket Equation\n");
+	printf("Advent of Code 2019\n");
+	printf("Day 1: The Tyranny of the Rocket Equation\n");
 
 	int32_t total_fuel = 0;
 	int32_t fuel_fuel = 0;
 
-    char buff[MAXBUFF];
-    while (fgets(buff, MAXBUFF, stdin)) {
-        int32_t mass = atoi(buff);
-        total_fuel += CALC_FUEL(mass);
-        fuel_fuel += fuel_for_fuel(mass);
-    }
-
+	char buff[MAXBUFF];
+	while (fgets(buff, MAXBUFF, stdin)) {
+		int32_t mass = atoi(buff);
+		total_fuel += CALC_FUEL(mass);
+		fuel_fuel += fuel_for_fuel(mass);
+	}
 
 	printf("Part 1: %d\n", total_fuel);
 	printf("Part 2: %d\n", fuel_fuel);
--- 2019/c/1902.c
+++ 2019/c/1902.c
@@ -1,60 +1,91 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
 
-#include "vector.h"
+#include "aoc_vector.h"
 
-#define MAXBUFF 8
-#define ungetchar(v) ungetc(v, stdin)
+// Part 1
+struct Int_vector {
+	size_t size;
+	size_t cap;
+	size_t elem_size;
+	int* vec;
+};
 
-int getint(int* pn)
+struct Int_vector intcode_run(const struct Int_vector* src, int noun, int verb)
 {
-    int c, sign;
+	struct Int_vector state;
+	aoc_vector_copy(state, *src);
 
-    while (isspace(c = getchar()))
-        ;
-    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
-        ungetchar(c);
-        return 0;
-    }
-    sign = (c == '-') ? -1 : 1;
-    if (c == '+' || c == '-')
-        c = getchar();
-    for (*pn = 0; isdigit(c); c = getchar())
-        *pn = 10 * *pn + (c - '0');
-    *pn *= sign;
-    if (c != EOF)
-        ungetchar(c);
-    return c;
+	state.vec[1] = noun;
+	state.vec[2] = verb;
+
+	int* p = state.vec;
+	while (*p != 99) {
+		switch (*p) {
+		case 1:
+			{
+				int x = state.vec[*(p+1)];
+				int y = state.vec[*(p+2)];
+				state.vec[*(p+3)] = x + y;
+				p += 4;
+			}
+			break;
+		case 2:
+			{
+				int x = state.vec[*(p+1)];
+				int y = state.vec[*(p+2)];
+				state.vec[*(p+3)] = x * y;
+				p += 4;
+			}
+			break;
+		}
+	}
+	return state;
 }
 
-int main(void)
+// Part 2
+struct Int_pair {
+	int noun;
+	int verb;
+};
+
+struct Int_pair brute_force_solve(const struct Int_vector* prog,
+		const int target)
 {
-    printf("Advent of Code 2019 - Day 2\n");
-    printf("1202 Program Alarm\n");
+	for (int i = 0; i < 100; ++i)
+		for (int j = 0; j < 100; ++j) {
+			struct Int_vector out = intcode_run(prog, i, j);
+			int zero = out.vec[0];
+			aoc_vector_free(out);
 
-    vector v;
-    vector_init(&v);
+			if (zero == target)
+				return (struct Int_pair){ i, j };
+		}
 
-    /*
-    for (int val = 0, c = 0; c != EOF && getint(&val) != EOF; ) {
-        vector_push(&v, val);
-        c = getchar();
-    }
-    */
-    char buff[MAXBUFF];
-    while (fgets(buff, MAXBUFF, stdin))
-        vector_push(&v, atoi(buff));
-
-	int32_t part1 = vector_size(&v);
-	int32_t part2 = 0;
-
-    for (int i = 0; i < vector_size(&v); ++i)
-        printf("%d ", vector_get(&v, i));
-    printf("\n");
-
-	printf("Part 1: %d\n", part1);
-	printf("Part 2: %d\n", part2);
-
-    vector_free(&v);
+	return (struct Int_pair){ -1, -1 };	// should never get here
 }
+
+#define calc_output(pair) 100 * (pair).noun + (pair).verb	// why not?
+
+int main()
+{
+	printf("Advent of Code 2019\n");
+	printf("Day 2: 1202 Program Alarm\n");
+
+	struct Int_vector vi;
+	aoc_vector_init(vi, int);
+
+	for (int n, c = 0; c != EOF && scanf("%d", &n) == 1; c = getchar())
+		aoc_vector_push(vi, n);
+
+	struct Int_vector end_state = intcode_run(&vi, 12, 2);
+	struct Int_pair inputs = brute_force_solve(&vi, 19690720);
+
+	printf("Part 1: %d\n", end_state.vec[0]);
+	printf("Part 2: %d\n", calc_output(inputs));
+
+	aoc_vector_free(vi);
+
+	return EXIT_SUCCESS;
+}
+
