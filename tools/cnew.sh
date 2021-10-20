aae3c6c097b7f5e24b3ca2947d73c9e069e7d12f
Chrinkus <seeschickrun@gmail.com>
2021-10-20 13:19:21 -0500

    Removed boilerplate file and made script
    Used a heredoc in the cnew.sh script to achieve same goal.

--- tools/cnew.sh
+++ tools/cnew.sh
@@ -1,16 +1,59 @@
 #!/bin/bash
 
-YEAR=$1
-DAY=$2
-TITLE=$3
+# cnew.sh
+#
+# Create a C file for an Advent of Code solution
+#
+# cnew -y 20 -d 1 -t "Report Repair"
+# - creates a file titled 2001.c in the current directory
+# - file is set up with header and result output ready to go
 
+while [ $# -ne 0 ]; do
+	case "$1" in
+		-y|--year)
+			YEAR="$2"
+			shift 2;;
+		-d|--day)
+			DAY="$2"
+			shift 2;;
+		-t|--title)
+			TITLE="$2"
+			shift 2;;
+	esac
+done
+
+# Left-pad single digit days
 if [ ${#DAY} -eq 1 ]; then
-	DAY="0${DAY}"
+	DAYFILE="0${DAY}"
+else
+	DAYFILE="${DAY}"
 fi
 
-FILE="day${DAY}.c"
+# Strip century from year
+if [ ${#YEAR} -eq 4 ]; then
+	YEARFILE="${YEAR:2:2}"
+else
+	YEARFILE="${YEAR}"
+	YEAR="20${YEAR}"
+fi
 
-cp boilerplate.c $FILE
+# File name
+FILE="${YEARFILE}${DAYFILE}.c"
 
-sed -i "s/YEAR/$YEAR/; s/DAY/$DAY/; s/TITLE/$TITLE/" $FILE
+# Boilerplate with placeholders
+cat << _EOF_ > $FILE
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("Advent of Code ${YEAR}\n");
+	printf("Day ${DAY}: ${TITLE}\n");
+
+	printf("Part 1: \n");
+	printf("Part 2: \n");
+
+	return EXIT_SUCCESS;
+}
+_EOF_
 
