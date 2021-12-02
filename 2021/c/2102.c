b76ab790e33ccb9ffc5818312909c8e5278ea140
Chris Schick <seeschickrun@gmail.com>
2021-12-02 00:13:56 -0600

    Hung a big switch off the unbraced for
    Also removed 'aim'. It was bugging me, I was sure the 'aim' would be
    used like an angle for the sub. In the end it was just a second 'y1'.

--- 2021/c/2102.c
+++ 2021/c/2102.c
@@ -8,25 +8,22 @@ int main()
 	printf("Advent of Code 2021\n");
 	printf("Day 02: Dive!\n");
 
-	int x = 0, y1 = 0, y2 = 0;
+	int x = 0, y1 = 0, y2 = 0;	// aim == y1
 
 	char buff[BUFFMAX];
-	for (int n, aim = 0; scanf("%s %d", buff, &n) == 2; ) {
+	for (int n; scanf("%s %d", buff, &n) == 2; )
 		switch (buff[0]) {
 		case 'f':
 			x += n;
-			y2 += aim * n;
+			y2 += y1 * n;
 			break;
 		case 'u':
 			y1 -= n;
-			aim -= n;
 			break;
 		case 'd':
 			y1 += n;
-			aim += n;
 			break;
 		}
-	}
 
 	printf("Part 1: %d\n", x * y1);
 	printf("Part 2: %d\n", x * y2);
