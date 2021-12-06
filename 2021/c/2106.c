861c418d03e48de56654fdee6257e8b1e639dd56
Chris Schick <seeschickrun@gmail.com>
2021-12-06 13:13:00 -0600

    Reworked solution to use globals..
    I NEVER use globals but so many of the solutions online have been so I'm
    just trying them out to see how they feel.
    
    Also, all that "spawn" talk seemed crass so I renamed the arrays.

--- 2021/c/2106.c
+++ 2021/c/2106.c
@@ -2,21 +2,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include <sxc_vector.h>
+#include <sxc_vector_types.h>
+
 #include "aoc_banner.h"
 
 enum {
-	SCHOOL_LEN = 7,
-	SPAWN_LEN = 9,
+	ADULTS_LEN = 7,
+	BABIES_LEN = 9,
 	PART1_DAYS = 80,
 	PART2_DAYS = 256
 };
 
-void advance_time(int64_t school[], int64_t spawn[], int days, int* day)
+// Globals just to see how it feels
+int64_t adults[ADULTS_LEN];
+int64_t babies[BABIES_LEN];
+
+void advance_time(int days, int* day)
 {
 	for ( ; *day < days; ++*day) {
-		int64_t tmp = spawn[*day % SPAWN_LEN];
-		spawn[*day % SPAWN_LEN] += school[*day % SCHOOL_LEN];
-		school[*day % SCHOOL_LEN] += tmp;
+		int64_t tmp = babies[*day % BABIES_LEN];
+		babies[*day % BABIES_LEN] += adults[*day % ADULTS_LEN];
+		adults[*day % ADULTS_LEN] += tmp;
 	}
 }
 
@@ -28,43 +35,36 @@ int64_t sum_array(int64_t a[], int len)
 	return sum;
 }
 
-int64_t tally_fish(int64_t school[], int64_t spawn[])
+int64_t tally_fish()
 {
-	return sum_array(school, SCHOOL_LEN) + sum_array(spawn, SPAWN_LEN);
+	return sum_array(adults, ADULTS_LEN) + sum_array(babies, BABIES_LEN);
 }
 
 int main()
 {
 	aoc_banner_2021("06", "Lanternfish");
 
-	int day = 0;
-	int64_t school[SCHOOL_LEN] = { 0 };
-	int64_t spawn[SPAWN_LEN] = { 0 };
-
 	// Read input
-	int size = 0, alloc = 8;
-	int* input = malloc(sizeof(int) * alloc);
-	for (int n; scanf("%d", &n) == 1; getchar()) {
-		if (size == alloc) {
-			alloc *= 2;
-			input = realloc(input, sizeof(int) * alloc);
-		}
-		input[size++] = n;
-	}
+	struct Int_vector input;
+	sxc_vector_init(&input);
+	for (int n; scanf("%d", &n) == 1; getchar())
+		sxc_vector_push(&input, n);
 
 	// Set initial state
-	for (int i = 0; i < size; ++i)
-		++school[input[i]];
-	free(input);
+	for (size_t i = 0; i < sxc_vector_size(&input); ++i)
+		++adults[sxc_vector_get(&input, i)];
+	sxc_vector_free(&input);
 
-	advance_time(school, spawn, PART1_DAYS, &day);
-	int64_t part1 = tally_fish(school, spawn);
+	int day = 0;
+	advance_time(PART1_DAYS, &day);
+	int64_t part1 = tally_fish();
 
-	advance_time(school, spawn, PART2_DAYS, &day);
-	int64_t part2 = tally_fish(school, spawn);
+	advance_time(PART2_DAYS, &day);
+	int64_t part2 = tally_fish();
 
 	aoc_report_ints(part1, part2);
 
 	return EXIT_SUCCESS;
 }
+// Globals feel dirty..
 
