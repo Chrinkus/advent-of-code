43f4973ec3fb52e0529179d0c1b51a9133839307
Chris Schick <seeschickrun@gmail.com>
2021-12-13 12:27:09 -0600

    Cleaned up solution.
    - Changed warning-causing find to use known vector values
    - Moved all magic chars to enum
    - Added the ability to fold a specific number of times for part 1

--- 2021/c/2113.c
+++ 2021/c/2113.c
@@ -6,18 +6,14 @@
 #include <sxc_string.h>
 #include <sxc_vector.h>
 
+enum Constants { DOT = '#', CLEAR = ' ', HORIZ = 'y', VERT = 'x', };
+
 struct Point {
 	int x;
 	int y;
 };
 
 SXC_NEW_VECTOR(Point_vector, struct Point);
-
-struct Graph {
-	char* graph;
-	int cols;
-	int rows;
-};
 
 struct Fold {
 	char axis;
@@ -26,9 +22,15 @@ struct Fold {
 
 SXC_NEW_VECTOR(Fold_vector, struct Fold);
 
-void mark_hash(const struct Point* p, struct Graph* g)
+struct Graph {
+	char* graph;
+	int cols;
+	int rows;
+};
+
+void mark_dot(const struct Point* p, struct Graph* g)
 {
-	g->graph[p->y * g->cols + p->x] = '#';
+	g->graph[p->y * g->cols + p->x] = DOT;
 }
 
 int fold_cmp(const struct Fold* f, char tar)
@@ -38,21 +40,27 @@ int fold_cmp(const struct Fold* f, char tar)
 
 void init_graph(struct Graph* g, const Fold_vector* folds)
 {
-	const struct Fold* f;
-	sxc_vector_find(folds, 'y', fold_cmp, f);
-	g->rows = f->index * 2 + 1;
-	sxc_vector_find(folds, 'x', fold_cmp, f);
-	g->cols = f->index * 2 + 1;
+	int rows = 0;
+	int cols = 0;
+	for (const struct Fold* f = sxc_vector_getp(folds, 0);
+			f && (!rows || !cols); ++f)
+		switch (f->axis) {
+		case HORIZ:	if (!rows) rows = f->index;	break;
+		case VERT:	if (!cols) cols = f->index;	break;
+		}
+
+	g->rows = rows * 2 + 1;
+	g->cols = cols * 2 + 1;
 	g->graph = malloc(sizeof(char) * g->rows * g->cols);
 }
 
-void draw_graph(struct Graph* g, const Point_vector* pts)
+void plot_graph(struct Graph* g, const Point_vector* pts)
 {
 	for (int i = 0; i < g->rows; ++i)
 		for (int j = 0; j < g->cols; ++j)
-			g->graph[i * g->cols + j] = ' ';
+			g->graph[i * g->cols + j] = CLEAR;
 
-	sxc_vector_foreach(pts, mark_hash, g);
+	sxc_vector_foreach(pts, mark_dot, g);
 }
 
 void print_graph(const struct Graph* g)
@@ -72,6 +80,7 @@ void print_fold(const struct Fold* f, void* data)
 }
 
 void collapse_horizontal(struct Graph* g, int index)
+	// Necessary for flat 2D array
 {
 	char* p = g->graph;
 	for (int i = 1; i < g->rows; ++i) {
@@ -81,23 +90,25 @@ void collapse_horizontal(struct Graph* g, int index)
 	}
 }
 
-void fold_graph(struct Graph* g, Fold_vector* folds)
+void fold_graph(struct Graph* g, Fold_vector* folds, size_t start, size_t stop)
 {
 	char* p = g->graph;
-	for (size_t i = 0; i < sxc_vector_size(folds); ++i) {
+	for (size_t i = start; i < stop; ++i) {
 		const struct Fold* f = sxc_vector_getp(folds, i);
-		if (f->axis == 'y') {
+		if (f->axis == HORIZ) {
+			// Fold along horizontal index
 			for (int i = g->rows-1, k = 0; i > f->index; --i, ++k)
 				for (int j = 0; j < g->cols; ++j)
-					if (p[i*g->cols+j] == '#')
-						p[k*g->cols+j] = '#';
+					if (p[i*g->cols+j] == DOT)
+						p[k*g->cols+j] = DOT;
 			g->rows = f->index;
 		} else {
+			// Fold along vertical index
 			for (int i = 0; i < g->rows; ++i)
 				for (int j = g->cols-1, k = 0; j > f->index;
 						--j, ++k)
-					if (p[i*g->cols+j] == '#')
-						p[i*g->cols+k] = '#';
+					if (p[i*g->cols+j] == DOT)
+						p[i*g->cols+k] = DOT;
 			collapse_horizontal(g, f->index);
 			g->cols = f->index;
 		}
@@ -109,7 +120,7 @@ int count_dots(const struct Graph* g)
 	int count = 0;
 	for (int i = 0; i < g->rows; ++i)
 		for (int j = 0; j < g->cols; ++j)
-			if (g->graph[i*g->cols+j] == '#')
+			if (g->graph[i*g->cols+j] == DOT)
 				++count;
 	return count;
 }
@@ -119,10 +130,10 @@ int main()
 	aoc_banner_2021("13", "Transparent Origami");
 
 	// Read points
-	Point_vector input;
-	sxc_vector_init(&input);
+	Point_vector dots;
+	sxc_vector_init(&dots);
 	for (struct Point p; scanf("%d,%d ", &p.x, &p.y) == 2; ) {
-		sxc_vector_push(&input, p);
+		sxc_vector_push(&dots, p);
 	}
 
 	// Read folds
@@ -132,17 +143,15 @@ int main()
 			scanf("fold along %c=%d ", &f.axis, &f.index) == 2; ) {
 		sxc_vector_push(&folds, f);
 	}
-	//sxc_vector_foreach(&folds, print_fold, NULL);
 
 	struct Graph graph;
 	init_graph(&graph, &folds);
+	plot_graph(&graph, &dots);
 
-	draw_graph(&graph, &input);
-
-	fold_graph(&graph, &folds);
-
-
+	fold_graph(&graph, &folds, 0, 1);
 	int part1 = count_dots(&graph);
+
+	fold_graph(&graph, &folds, 1, sxc_vector_size(&folds));
 	const char* part2 = "See print-out below:\n";
 
 	printf(TCINV "Part 1:" TCRINV " %d\n", part1);
@@ -151,7 +160,7 @@ int main()
 	print_graph(&graph);
 
 	free(graph.graph);
-	sxc_vector_free(&input);
+	sxc_vector_free(&dots);
 	sxc_vector_free(&folds);
 
 	return EXIT_SUCCESS;
