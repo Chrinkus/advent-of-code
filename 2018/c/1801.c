38837c8564e073630bc79e921142c00e8a36f9d4
Chris Schick <seeschickrun@gmail.com>
2022-02-02 21:10:01 -0600

    Day 1 complete
    - Depends on libcgs

--- 2018/c/1801.c
+++ 2018/c/1801.c
@@ -1,14 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <cgs/cgs.h>
+
+CGS_ARRAY_DEFINE_STRUCT(int_array, int);
+
+int int_cmp(const void* a, const void* b)
+{
+	return *(int*)a - *(int*)b;
+}
+
+int* check_repeat_freq(struct cgs_bst* freqs, int freq, int* found)
+{
+	struct cgs_variant var = { 0 };
+	cgs_variant_set_int(&var, freq);
+
+	if (cgs_bst_search(freqs, &var)) {
+		*found = freq;
+		return found;
+	}
+	cgs_bst_insert(freqs, &var);
+	return NULL;
+}
+
 int main(void)
 {
 	printf("Advent of Code 2018 Day 1: Chronal Calibration\n");
 
 	int part1 = 0;
 	int part2 = 0;
-	for (int n; scanf(" %d", &n) == 1; )
+
+	struct cgs_bst* freqs = cgs_bst_new(int_cmp);
+
+	struct int_array shifts;
+	cgs_array_init(&shifts);
+
+	for (int n; scanf(" %d", &n) == 1; ) {
+		cgs_array_push(&shifts, n);
 		part1 += n;
+		if (!part2)
+			check_repeat_freq(freqs, part1, &part2);
+	}
+
+	for (int freq = part1; 1; ) {
+		for (size_t i = 0; i < shifts.len; ++i) {
+			freq += cgs_array_get(&shifts, i);
+			if (check_repeat_freq(freqs, freq, &part2))
+				goto getout;
+		}
+	}
+getout:
+	cgs_array_free(&shifts);
+	cgs_bst_free(freqs);
 
 	printf("Part 1: %d\n", part1);
 	printf("Part 2: %d\n", part2);
