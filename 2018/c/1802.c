9387e416a697bbc8846d23d2dead29de367e9c84
Chrinkus <seeschickrun@gmail.com>
2022-03-07 20:59:10 -0600

    WIP experimenting with array iterators

--- 2018/c/1802.c
+++ 2018/c/1802.c
@@ -24,6 +24,13 @@ void count_repeats(const char* s, int* twice, int* thrice)
 	*thrice += trips;
 }
 
+const char* find_common_ids(struct cgs_array* ids)
+{
+	cgs_array_sort(ids, cgs_str_cmp);
+
+	return "";
+}
+
 int main(void)
 {
 	printf("Advent of Code 2018 Day 2: Inventory Management System\n");
@@ -31,18 +38,27 @@ int main(void)
 	int twice = 0;
 	int thrice = 0;
 
+	struct cgs_array* ids = cgs_array_new(char*);
 	struct cgs_string* buff = cgs_string_new();
 	for ( ; cgs_io_getline(stdin, buff) > 0; cgs_string_clear(buff)) {
+		// Part 2
+		char* p = cgs_strdup(cgs_string_read(buff));
+		cgs_array_push(ids, &p);
+
+		// Part 1
 		cgs_string_sort(buff);
 		count_repeats(cgs_string_read(buff), &twice, &thrice);
 	}
 	cgs_string_free(buff);
 
 	int part1 = twice * thrice;
-	int part2 = 0;
+
+	const char* part2 = find_common_ids(ids);
 
 	printf("Part 1: %d\n", part1);
-	printf("Part 2: %d\n", part2);
+	printf("Part 2: %s\n", part2);
+
+	cgs_array_free(ids);
 
 	return EXIT_SUCCESS;
 }
