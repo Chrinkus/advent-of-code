b6172b902df1c7ecab74e6f52b720cf7f5ebed31
Chris Schick <seeschickrun@gmail.com>
2022-06-08 11:32:42 -0500

    Updated 1803 and forced fruity usage
    - Wanted another use-case for fruity. This solution didn't benefit from
      the dynamic nature of fruity but it used some algorithms.
    - This ends up as a longer source file..
    - Is it an improvement over the previous version? Not sure, but it was
      fun to off-load some of the nested for loops..

--- 2018/c/1803.c
+++ 2018/c/1803.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include <cgs/cgs.h>
+#include <fruity.h>
 
 enum { MAX_SIDE = 1000 };
 
@@ -9,52 +10,91 @@ struct claim {
 	int id, x, y, w, h;
 };
 
-int count_multi_claims(const int arr[][MAX_SIDE])
+void* read_input(struct cgs_array* input)
 {
-	int count = 0;
-	for (int i = 0; i < MAX_SIDE; ++i)
-		for (int j = 0; j < MAX_SIDE; ++j)
-			count += arr[i][j] > 1;
-	return count;
+        for (struct claim c; scanf("#%d @ %d,%d: %dx%d ",
+                                &c.id, &c.x, &c.y, &c.w, &c.h) == 5; )
+                if (!cgs_array_push(input, &c))
+                        return NULL;
+        return input;
 }
 
-int find_solo_claim(const struct cgs_array* claims, const int fab[][MAX_SIDE])
+void increment_claim(const void* element, size_t i, void* data)
 {
-	for (size_t i = 0; i < cgs_array_length(claims); ++i) {
-		const struct claim* p = cgs_array_get(claims, i);
-		for (int j = p->y; j < p->y + p->h; ++j)
-			for (int k = p->x; k < p->x + p->w; ++k)
-				if (fab[j][k] > 1)
-					goto next_claim;
-		return p->id;
-next_claim:
-                continue;
-	}
-	return -1;
+        const struct claim* c = element;
+        int** fabric = data;
+        (void)i;
+
+        for (int i = c->y, h = c->y + c->h; i < h; ++i)
+                for (int j = c->x, w = c->x + c->w; j < w; ++j)
+                        ++fabric[i][j];
+}
+
+void* init_grid_and_mark_claims(Fruity2D* g, const struct cgs_array* input)
+{
+        if (!fruity_new(g, MAX_SIDE, MAX_SIDE, sizeof(int)))
+                return NULL;
+
+        int n = 0;
+        fruity_init(g, &n);
+
+        cgs_array_foreach(input, increment_claim, fruity_data(g));
+
+        return g;
+}
+
+int is_multi_claim(Fruity2DCell cell, void* data)
+{
+        const int* p = cell.ptr;
+        (void)data;
+
+        return *p > 1;
+}
+
+int is_solo_claim(const void* element, const void* data)
+{
+        const struct claim* c = element;
+        const int*const* fabric = data;
+
+        for (int i = c->y, h = i + c->h; i < h; ++i)
+                for (int j = c->x, w = j + c->w; j < w; ++j)
+                        if (fabric[i][j] > 1)
+                                return CGS_FALSE;
+        return CGS_TRUE;
+}
+
+int get_solo_claim_id(struct cgs_array* input, const int** fabric)
+{
+        struct claim* solo = cgs_array_find(input, is_solo_claim, fabric);
+        if (solo)
+                return solo->id;
+
+        return -1;
 }
 
 int main(void)
 {
 	printf("Advent of Code 2018 Day 3: No Matter How You Slice It\n");
 
-	int fabric[MAX_SIDE][MAX_SIDE] = { 0 };
-	struct cgs_array* claims = cgs_array_new(struct claim);
+        struct cgs_array input = { 0 };
+        if (!cgs_array_new(&input, sizeof(struct claim)))
+                return EXIT_FAILURE;
 
-	for (struct claim c; scanf("#%d @ %d,%d: %dx%d ",
-				&c.id, &c.x, &c.y, &c.w, &c.h) == 5; ) {
-		cgs_array_push(claims, &c);
-		for (int i = c.y; i < c.y + c.h; ++i)
-			for (int j = c.x; j < c.x + c.w; ++j)
-				++fabric[i][j];
-	}
+        if (!read_input(&input))
+                return EXIT_FAILURE;
 
-	int part1 = count_multi_claims(fabric);
-	int part2 = find_solo_claim(claims, fabric);
+        Fruity2D grid = { 0 };
+        if (!init_grid_and_mark_claims(&grid, &input))
+                return EXIT_FAILURE;
+
+	int part1 = fruity_count_if(&grid, is_multi_claim, NULL);
+	int part2 = get_solo_claim_id(&input, fruity_data(&grid));
 
 	printf("Part 1: %d\n", part1);
 	printf("Part 2: %d\n", part2);
 
-	cgs_array_free(claims);
+        cgs_array_free(&input);
+        fruity_free(&grid);
 
 	return EXIT_SUCCESS;
 }
