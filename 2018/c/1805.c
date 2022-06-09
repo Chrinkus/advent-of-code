54c80793502af2a73d58449e4fc1cea5bfd9071f
Chris Schick <seeschickrun@gmail.com>
2022-06-09 13:45:44 -0500

    Day 5 brought up to new standard.

--- 2018/c/1805.c
+++ 2018/c/1805.c
@@ -7,73 +7,98 @@
 
 enum { NUM_TRIALS = 26 };
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * Part 1
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
 int reaction(char a, char b)
 {
-	return (islower(a) && b == toupper(a))
-		|| (isupper(a) && b == tolower(a));
+        return (islower(a) && b == toupper(a))
+                || (isupper(a) && b == tolower(a));
 }
 
 void reduce_polymer(char* polymer)
 {
-	const char* const start = polymer;
-	for (const char* p = start, *s = p+1; *p; ++s) {
-		if (!*s) {
-			*polymer++ = *p++;
-		} else if (reaction(*p, *s)) {
-			p = p == start || polymer == start ? ++s : --polymer;
-		} else {
-			*polymer++ = *p;
-			p = s;
-		}
-	}
-	*polymer = '\0';
+        const char* const start = polymer;
+
+        for (const char* p = polymer, *s = p+1; *p; ++s) {
+                if (!*s) {
+                        *polymer++ = *p++;
+                } else if (reaction(*p, *s)) {
+                        p = p == start || polymer == start ? ++s : --polymer;
+                } else {
+                        *polymer++ = *p;
+                        p = s;
+                }
+        }
+        *polymer = '\0';
 }
 
-void prepare_polymer(const char* base, char lo, char* polymer)
+int get_reacted_length(const char* polymer)
 {
-	char up = toupper(lo);
-	for (const char* r = base; *r; ++r)
-		if (*r != lo && *r != up)
-			*polymer++ = *r;
-	*polymer = '\0';
+        char* tmp = cgs_strdup(polymer);
+        if (!tmp)
+                return -1;
+
+        reduce_polymer(tmp);
+
+        int len = strlen(tmp);
+        free(tmp);
+        return len;
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * Part 2
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+void prepare_polymer(const char* base, char lo, char* out)
+{
+        char up = toupper(lo);
+        for (const char* r = base; *r; ++r)
+                if (*r != lo && *r != up)
+                        *out++ = *r;
+        *out = '\0';
 }
 
 int find_max_collapse(const char* polymer, int max)
 {
-	char* buff = malloc(strlen(polymer) + 1);
+        char* buff = malloc(strlen(polymer) + 1);
+        if (!buff)
+                return -1;
 
-	int trials[NUM_TRIALS] = { 0 };
+        int trials[NUM_TRIALS] = { 0 };
 
-	for (int i = 0; i < NUM_TRIALS; ++i) {
-		prepare_polymer(polymer, 'a' + i, buff);
-		reduce_polymer(buff);
-		trials[i] = strlen(buff);
-	}
-	free(buff);
+        for (int i = 0; i < NUM_TRIALS; ++i) {
+                prepare_polymer(polymer, 'a' + i, buff);
+                reduce_polymer(buff);
+                trials[i] = strlen(buff);
+        }
+        free(buff);
 
-	int min = max;
-	for (int i = 0; i < NUM_TRIALS; ++i)
-		if (trials[i] < min)
-			min = trials[i];
-	return min;
+        int min = max;
+        for (int i = 0; i < NUM_TRIALS; ++i)
+                if (trials[i] < min)
+                        min = trials[i];
+        return min;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * Main
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
 int main(void)
 {
-	printf("Advent of Code 2018 Day 5: Alchemical Reduction\n");
+        printf("Advent of Code 2018 Day 5: Alchemical Reduction\n");
 
-	char* polymer = cgs_io_readline(stdin);
+        char* polymer = cgs_io_readline(stdin);
+        if (!polymer)
+                return EXIT_FAILURE;
 
-	reduce_polymer(polymer);
+        int part1 = get_reacted_length(polymer);
+        int part2 = find_max_collapse(polymer, part1);
 
-	int part1 = strlen(polymer);
-	int part2 = find_max_collapse(polymer, part1);
+        printf("Part 1: %d\n", part1);
+        printf("Part 2: %d\n", part2);
 
-	printf("Part 1: %d\n", part1);
-	printf("Part 2: %d\n", part2);
+        free(polymer);
 
-	free(polymer);
-
-	return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
 
