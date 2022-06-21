b0b25230609577ed65a28fae4f3386bce436092f
Chris Schick <seeschickrun@gmail.com>
2022-06-21 17:02:09 -0500

    Cleaned up day 10
    - moved frame to sky-specific code
    - fixed uncaught bug of extent initialization
    - streamlined main

--- 2018/c/1810.c
+++ 2018/c/1810.c
@@ -19,15 +19,24 @@ struct extents {
         int min, max;
 };
 
+struct extents extents_init(void)
+{
+        return (struct extents){ .min = INT_MAX, .max = INT_MIN, };
+}
+
+int extents_diff(const struct extents* ext)
+{
+        return ext->max - ext->min;
+}
+
 struct frame {
         struct extents h;
         struct extents w;
 };
 
-void frame_init(struct frame* f)
+struct frame frame_init(void)
 {
-        f->h.min = f->w.min = INT_MAX;
-        f->h.max = f->w.max = INT_MIN;
+        return (struct frame){ .h = extents_init(), .w = extents_init(), };
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
@@ -36,8 +45,8 @@ void frame_init(struct frame* f)
 void* read_input(struct cgs_array* stars)
 {
         const char* fmt = " position=< %d, %d> velocity=< %d, %d> ";
-        for (struct light v; scanf(fmt, &v.x, &v.y, &v.dx, &v.dy) == 4; )
-                if (!cgs_array_push(stars, &v))
+        for (struct light l; scanf(fmt, &l.x, &l.y, &l.dx, &l.dy) == 4; )
+                if (!cgs_array_push(stars, &l))
                         return NULL;
         return stars;
 }
@@ -45,62 +54,74 @@ void* read_input(struct cgs_array* stars)
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
  * Part 1 and 2 - Increment lights until message appears
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
-void increment_positions(struct light* v, struct extents* ex)
+void increment_and_track_positions(struct light* l, struct extents* ex)
 {
-        v->x += v->dx;
-        v->y += v->dy;
+        l->x += l->dx;
+        l->y += l->dy;
 
-        ex->min = CGS_MIN(v->y, ex->min);
-        ex->max = CGS_MAX(v->y, ex->max);
+        ex->min = CGS_MIN(l->y, ex->min);
+        ex->max = CGS_MAX(l->y, ex->max);
 }
 
-void decrement_positions(struct light* v, struct frame* f)
+void increment_and_track_cb(void* ele, size_t i, void* data)
 {
-        v->x -= v->dx;
-        v->y -= v->dy;
-
-        f->h.min = CGS_MIN(v->y, f->h.min);
-        f->h.max = CGS_MAX(v->y, f->h.max);
-        f->w.min = CGS_MIN(v->x, f->w.min);
-        f->w.max = CGS_MAX(v->x, f->w.max);
-}
-
-void increment_cb(void* ele, size_t i, void* data)
-{
-        struct light* v = ele;
+        struct light* l = ele;
         struct extents* ex = data;
         (void)i;
-        increment_positions(v, ex);
+        increment_and_track_positions(l, ex);
+}
+
+void decrement_positions(struct light* l)
+{
+        l->x -= l->dx;
+        l->y -= l->dy;
 }
 
 void decrement_cb(void* ele, size_t i, void* data)
 {
-        struct light* v = ele;
-        struct frame* f = data;
+        struct light* l = ele;
         (void)i;
-        decrement_positions(v, f);
+        (void)data;
+        decrement_positions(l);
 }
 
-int wait_for_message(struct cgs_array* stars, struct frame* frame)
+int wait_for_message(struct cgs_array* stars)
 {
         int seconds = 0;
-        for (int hp = INT_MAX, hc = hp - 1; hc < hp; ++seconds) {
-                struct extents ex = { 0 };
-                cgs_array_transform(stars, increment_cb, &ex);
-                hp = hc;
-                hc = ex.max - ex.min;
+        for (int hprev = INT_MAX, hcurr = hprev - 1; hcurr < hprev; ) {
+                struct extents hext = extents_init();
+                cgs_array_transform(stars, increment_and_track_cb, &hext);
+                hprev = hcurr;
+                hcurr = extents_diff(&hext);
+                ++seconds;
         }
-        cgs_array_transform(stars, decrement_cb, frame);
+        cgs_array_transform(stars, decrement_cb, NULL); // back up 1s
         return --seconds;
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
  * Print the sky
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+void set_frame(struct frame* f, const struct light* l)
+{
+        f->h.min = CGS_MIN(l->y, f->h.min);
+        f->h.max = CGS_MAX(l->y, f->h.max);
+        f->w.min = CGS_MIN(l->x, f->w.min);
+        f->w.max = CGS_MAX(l->x, f->w.max);
+}
+
+void set_frame_cb(const void* ele, size_t i, void* data)
+{
+        const struct light* light = ele;
+        struct frame* frame = data;
+        (void)i;
+        set_frame(frame, light);
+}
+
 void* init_sky(Fruity2D* sky, const struct frame* frame)
 {
-        int rows = frame->h.max - frame->h.min + 1;
-        int cols = frame->w.max - frame->w.min + 1;
+        int rows = extents_diff(&frame->h) + 1;
+        int cols = extents_diff(&frame->w) + 1;
         void* res = fruity_new(sky, rows, cols, sizeof(char));
         if (!res)
                 return NULL;
@@ -111,17 +132,23 @@ void* init_sky(Fruity2D* sky, const struct frame* frame)
         return res;
 }
 
-void plot_stars(Fruity2D* sky, const struct cgs_array* stars,
-                const struct frame* f)
+void* plot_stars(Fruity2D* sky, const struct cgs_array* stars)
 {
-        int xoff = f->w.min;
-        int yoff = f->h.min;
+        struct frame frame = frame_init();
+        cgs_array_foreach(stars, set_frame_cb, &frame);
+
+        if (!init_sky(sky, &frame))
+                return NULL;
+
+        int xoff = frame.w.min;
+        int yoff = frame.h.min;
 
         char** s = fruity_data(sky);
         for (size_t i = 0; i < cgs_array_length(stars); ++i) {
                 const struct light* l = cgs_array_get(stars, i);
                 s[l->y - yoff][l->x - xoff] = STAR;
         }
+        return sky;
 }
 
 void print_sky(const Fruity2D* sky)
@@ -145,19 +172,14 @@ int main(void)
         if (!read_input(&stars))
                 return EXIT_FAILURE;
 
-        struct frame frame = { 0 };
-        frame_init(&frame);
-        int part2 = wait_for_message(&stars, &frame);
+        int part2 = wait_for_message(&stars);
 
         Fruity2D sky = { 0 };
-        if (!init_sky(&sky, &frame))
+        if (!plot_stars(&sky, &stars))
                 return EXIT_FAILURE;
-
-        plot_stars(&sky, &stars, &frame);
 
         printf("Part 1:\n");
         print_sky(&sky);
-        printf("\n");
         printf("Part 2: %d\n", part2);
 
         cgs_array_free(&stars);
