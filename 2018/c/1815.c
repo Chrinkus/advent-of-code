68783ccc0378d4174ea899ba7d37c2173959627a
Chris Schick <seeschickrun@gmail.com>
2022-07-21 22:24:30 -0500

    Added print functionality and tweaks
    - improved error messaging in initter
    - added compare function for sorting

--- 2018/c/1815.c
+++ 2018/c/1815.c
@@ -23,8 +23,17 @@ struct entity {
         int y;
 };
 
+int entity_cmp(const void* a, const void* b)
+{
+        const struct entity* e1 = a;
+        const struct entity* e2 = b;
+
+        int res = e1->y - e2->y;
+        return (res == 0) ? e1->x - e2->x : res;
+}
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
- * Input Processing
+ * Input Processing and Setup
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
 void* read_input(struct cgs_array* input)
 {
@@ -68,16 +77,22 @@ void* read_input_and_init_structs(Fruity2D* cave, struct cgs_array* ents)
         if (!read_input(&input))
                 return cgs_error_retnull("read_input");
 
-        if (!cgs_array_new(ents, sizeof(struct entity)))
+        if (!cgs_array_new(ents, sizeof(struct entity))) {
+                cgs_error_msg("cgs_array_new: ents");
                 goto error_cleanup;
+        }
 
         int rows = cgs_array_length(&input);
         int cols = strlen(*(const char*const*)cgs_array_get(&input, 0));
-        if (!fruity_new(cave, rows, cols, sizeof(char)))
+        if (!fruity_new(cave, rows, cols, sizeof(char))) {
+                cgs_error_msg("fruity_new");
                 goto error_cleanup;
+        }
 
-        if (!init_cave_and_ents(cave, ents, &input))
+        if (!init_cave_and_ents(cave, ents, &input)) {
+                cgs_error_msg("init_cave_and_ents");
                 goto error_cleanup;
+        }
 
         cgs_array_free_all(&input);
         return ents;
@@ -85,6 +100,42 @@ void* read_input_and_init_structs(Fruity2D* cave, struct cgs_array* ents)
 error_cleanup:
         cgs_array_free_all(&input);
         return NULL;
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * Print the Cave
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+void print_cave(const Fruity2D* cave)
+{
+        fruity_foreach(cave,
+                       fruity_io_newline, NULL,
+                       fruity_io_print_char, NULL);
+}
+
+void print_entity(const void* a, size_t i, void* data)
+{
+        (void)i;
+        (void)data;
+        const struct entity* e = a;
+
+        printf("%s (%2d,%2d) hp = %d\n",
+                        e->type == TYPE_ELF ? "ELF" : "GOB",
+                        e->x, e->y, e->hp);
+}
+
+void print_entities(const struct cgs_array* ents)
+{
+        cgs_array_foreach(ents, print_entity, NULL);
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * Run simulation
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+void battle(Fruity2D* cave, struct cgs_array* ents)
+{
+        cgs_array_sort(ents, entity_cmp);
+        print_entities(ents);
+        print_cave(cave);
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
@@ -98,6 +149,8 @@ int main(void)
         struct cgs_array entities = { 0 };
         if (!read_input_and_init_structs(&cave, &entities))
                 return cgs_error_retfail("read_input_and_init_structs");
+
+        battle(&cave, &entities);
 
 	int part1 = 0;
 	int part2 = 0;
