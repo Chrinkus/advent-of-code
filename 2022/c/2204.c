de6f295e335850e1957cda500b95187d73fa4530
Chris Schick <seeschickrun@gmail.com>
2022-12-04 11:05:07 -0600

    Remembered boolean addition.

--- 2022/c/2204.c
+++ 2022/c/2204.c
@@ -5,10 +5,8 @@ static void
 count_section_overlaps(int* p1, int* p2)
 {
         for (int a, b, c, d; scanf("%d-%d,%d-%d", &a, &b, &c, &d) == 4; ) {
-                if ((a >= c && b <= d) || (c >= a && d <= b))
-                        ++*p1;
-                if ((a <= d && b >= c) || (c <= b && d >= a))
-                        ++*p2;
+                *p1 += ((a >= c && b <= d) || (c >= a && d <= b));
+                *p2 += ((a <= d && b >= c) || (c <= b && d >= a));
         }
 }
 
@@ -24,13 +22,13 @@ int main(void)
 
         return EXIT_SUCCESS;
 }
-/* Golf'd
+/*
 #include <stdio.h>
 int main(void) {
         int p1 = 0, p2 = 0;
         for (int a,b,c,d; scanf("%d-%d,%d-%d", &a,&b,&c,&d) == 4; ) {
-                if ((a >= c && b <= d) || (c >= a && d <= b))   ++p1;
-                if ((a <= d && b >= c) || (c <= b && d >= a))   ++p2;
+                p1 += ((a >= c && b <= d) || (c >= a && d <= b));
+                p2 += ((a <= d && b >= c) || (c <= b && d >= a));
         }
         printf("%d\n%d\n", p1, p2);
 }
